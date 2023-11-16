265e1f30eb9a71b8a26f0b462afb44450d7beac1
nesmond <nesmond11@gmail.com>
2023-11-16 15:20:18 +0530

    kl

--- src/App.jsx
+++ src/App.jsx
@@ -28,8 +28,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-      hjhuhui
-      kkkk
+     1 first 
     </>
   )
 }
